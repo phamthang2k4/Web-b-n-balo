2af39b4e0564405e554e006b9c77e525dcebb5e7
phamthang2k4 <phamthang6636@gmail.com>
2024-09-16 15:44:20 +0700

    Bài 4. Tạo dynamic menu bằng Component

---
+++ ThucHanhWebMVC/Repository/ILoaiSpRepository.cs
@@ -0,0 +1,12 @@
+﻿using ThucHanhWebMVC.Models;
+namespace ThucHanhWebMVC.Repository
+{
+    public interface ILoaiSpRepository
+    {
+        TLoaiSp Add(TLoaiSp loaiSp);
+        TLoaiSp Update(TLoaiSp loaiSp);
+        TLoaiSp Delete(String maloaiSp);
+        TLoaiSp Get(String maloaiSp);
+        IEnumerable<TLoaiSp> GetAllLoaiSp();
+    }
+}
