f88e85c496f1e3bb29a5f8cae35c1e62c238ae27
phamthang2k4 <phamthang6636@gmail.com>
2024-09-18 11:08:47 +0700

    Bài 6. Xây dựng trang chi tiết sản phẩm bằng ViewBag

--- ThucHanhWebMVC/obj/Debug/net8.0/ThucHanhWebMVC.AssemblyInfo.cs
+++ ThucHanhWebMVC/obj/Debug/net8.0/ThucHanhWebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThucHanhWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2af39b4e0564405e554e006b9c77e525dcebb5e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71801c8f1df97a4f4b279f439ffdebaf13eb36e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThucHanhWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThucHanhWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
